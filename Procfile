80b8cac7121f2e058eea375e16de03aeb46145ec
nkhaja <nbl.khaja@gmail.com>
2016-11-16 12:27:32 -0800

    Commit before deploying to heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn gettingstarted.wsgi
-web: gunicorn hello:app
+web: python app.py
--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def hello():
     return corpus_processing.generateSentence()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port=port)
