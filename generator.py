2325c8a42c9bbd4936b1395d7fccd71de57a4486
nkhaja <nbl.khaja@gmail.com>
2016-12-13 21:55:17 -0800

    working on markov chain

--- generator.py
+++ generator.py
@@ -1,13 +1,11 @@
 from histograms import Dictogram
 from probabilityModel import StochasticSample
 import refinedCorpus
-
+import codecs
 def main():
     filename = "GoT.txt"
-    txt = open(filename)
-    cleanText = refinedCorpus.refineText(txt)
+    cleanText = refinedCorpus.refineText(filename)
     histogram = Dictogram(cleanText)
     stochasticSampler = StochasticSample(histogram)
 
     return stochasticSampler.generateSentence()
-
--- refinedCorpus.py
+++ refinedCorpus.py
@@ -1,9 +1,16 @@
+from HTMLParser import HTMLParser
+#python 3 from html.parser import HTMLParser
+import codecs
 def refineText(txt):
-    filteredText = txt.read()
+    # h = HTMLParser()
+    txt_as_string = codecs.open(txt,encoding = 'utf-8')#open(filename)
+    txt_as_string = txt_as_string.read()
+    filteredText = txt_as_string.encode('ascii','ignore')
     removeCharacters = ['\n', '','.','!','?','\"', '\"',',','[',']','-',"{", "}", ';', '\t']
 
     for ch in removeCharacters:
         filteredText = filteredText.replace(ch, "")
+
 
     words = filteredText.split()
     return words
