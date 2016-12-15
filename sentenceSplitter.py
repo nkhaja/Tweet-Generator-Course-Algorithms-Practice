5b09191baa70bcc664a7cbd8816747c425f85014
nkhaja <nbl.khaja@gmail.com>
2016-12-15 10:21:37 -0800

    testing nltk library

--- sentenceSplitter.py
+++ sentenceSplitter.py
@@ -2,15 +2,15 @@ import refinedCorpus
 import nltk
 import codecs
 
-txt = "GoT.txt"
-txt_as_string = codecs.open(txt,encoding = 'utf-8')#open(filename)
-txt_as_string = txt_as_string.read()
-filteredText = txt_as_string.encode('ascii','ignore')
+# txt = "GoT.txt"
+# txt_as_string = codecs.open(txt,encoding = 'utf-8')#open(filename)
+# txt_as_string = txt_as_string.read()
+# filteredText = txt_as_string.encode('ascii','ignore')
 
-sentence = "I went to the store"
-tokenizer = nltk.data.load(filteredText)
-tokens = tokenizer.tokenize(filteredText)
-
-index = 0
-for i in range(200):
-    print tokens[i]
+# sentence = ["""At eight o'clock on Thursday morning
+# ... Arthur didn't feel very good."""]
+# tokens = nltk.word_tokenize(sentence)
+#
+# index = 0
+# for i in range(len(tokens)):
+#     print tokens[i]
