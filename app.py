8debad2946e0fb68c87a625199179ecdbaf378b3
nkhaja <nbl.khaja@gmail.com>
2017-01-25 23:37:11 -0800

    image resizing modified

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ def tweet():
     print(status)
     return redirect('/')
 
+@app.route('/generate', methods=['GET'])
+def generate():
+    tweet_text = generator.main()
+    if len(tweet_text) > 140:
+        tweet_text = tweet_text[0:139]
+    return render_template('tweet.html',tweet_text=tweet_text)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0', port=port)
